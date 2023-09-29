4ffb0c27085eef796e2bb841c6671f94a9e8725b
shabilramadhan <shabilramadhan31@gmail.com>
2023-09-29 08:44:47 +0700

    Tugas03 Percabangan

---
+++ if1.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    
+    cin >> n;
+    
+    if (n == 50) {
+        cout << "SETENGAH" << endl;
+    }
+}
