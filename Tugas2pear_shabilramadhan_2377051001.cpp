7eab1db9d8298a44b6f9e488535526bfbced7a1c
shabilramadhan <shabilramadhan31@gmail.com>
2023-10-20 20:01:23 +0700

    Tugas1peargroup

---
+++ Tugas2pear_shabilramadhan_2377051001.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char repeat = 'y';
+    int counter = 0;
+
+    while(repeat == 'y'){
+        cout << "Apakah kamu sayang dia ?:";
+        cin >> repeat;
+        counter ++;
+    }
+    cout << "----------"<< endl;
+    cout << "kamu masih punya harapan" << endl;
+    cout << "setidaknya kamu sudah mencoba sebanyak" << counter << "kali" << endl;;
+    return 0;
+}
